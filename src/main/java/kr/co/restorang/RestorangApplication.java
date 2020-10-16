94cd3adf3aebd82dfdcebfd1436ce7cfd1191278
Ivanluca <jungjihoonkr97@gmail.com>
2020-10-16 10:05:43 +0700

    init project

---
+++ src/main/java/kr/co/restorang/RestorangApplication.java
@@ -0,0 +1,13 @@
+package kr.co.restorang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestorangApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestorangApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kr/co/restorang/RestorangApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.restorang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestorangApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
