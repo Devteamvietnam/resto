366c3ab26ebde13bebeb94cf0d6a7a20d1af8136
Ivanluca <jungjihoonkr97@gmail.com>
2020-10-18 13:57:18 +0700

    add: Add ArMenu API

--- src/main/java/kr/co/restorang/services/MenuServiceListImpl.java
+++ src/main/java/kr/co/restorang/services/MenuServiceListImpl.java
@@ -7,8 +7,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import kr.co.restorang.entity.ArImageEntity;
+import kr.co.restorang.entity.ArMenuEntity;
 import kr.co.restorang.entity.MenuImageEntity;
 import kr.co.restorang.repository.ArImageRepository;
+import kr.co.restorang.repository.ArMenuRepository;
 import kr.co.restorang.repository.MenuImageRepository;
 
 @Service
@@ -18,7 +20,11 @@ public class MenuServiceListImpl implements MenuServiceList{
 	ArImageRepository arimageRepository;
 	
 	@Autowired
+	ArMenuRepository armenuRepository;
+	
+	@Autowired
 	MenuImageRepository menuimageRepository;
+	//AR IMAGE
 	@Override
 	public List<ArImageEntity> loadAllArImage() {
 		return arimageRepository.findAll();
@@ -34,22 +40,42 @@ public class MenuServiceListImpl implements MenuServiceList{
 	public ArImageEntity saveArImage(ArImageEntity arimage) {
 		return arimageRepository.save(arimage);
 	}
+	//END AR IMAGE
+  
+	// AR MENU
+
 	@Override
-	public void delete(String id) {
-		arimageRepository.deleteById(id);
-		
+	public List<ArMenuEntity> loadAllArMenu() {
+		return armenuRepository.findAll();
 	}
 
 	@Override
-	public MenuImageEntity saveImage(MenuImageEntity img) {
-		return menuimageRepository.save(img);
+	public Optional<ArMenuEntity> getArMenu(String id) {
+		return armenuRepository.findById(id);
 	}
 
 	@Override
-	public Optional<MenuImageEntity> findImageById(String id) {
-		return menuimageRepository.findById(id);
+	public ArMenuEntity saveArMenu(ArMenuEntity imgMe) {
+		return armenuRepository.save(imgMe);
 	}
 
+// END AR MENU
+	
+	// Chung
+		@Override
+		public MenuImageEntity saveImage(MenuImageEntity img) {
+			return menuimageRepository.save(img);
+		}
 
+		@Override
+		public Optional<MenuImageEntity> findImageById(String id) {
+			return menuimageRepository.findById(id);
+		}
+		@Override
+		public void delete(String id) {
+			armenuRepository.deleteById(id);
+			
+		}
+	// END chung
 
 }
